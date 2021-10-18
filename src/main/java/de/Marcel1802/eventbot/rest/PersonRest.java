905781ffc7d84aa5e199c970788171fd3929ff87
Marcel Eichler <marcel.eichler@outlook.com>
2021-10-18 13:13:10 +0200

    Added permission request to personRest

--- src/main/java/de/Marcel1802/eventbot/rest/PersonRest.java
+++ src/main/java/de/Marcel1802/eventbot/rest/PersonRest.java
@@ -2,8 +2,13 @@ package de.Marcel1802.eventbot.rest;
 
 import de.Marcel1802.eventbot.entities.Person;
 import de.Marcel1802.eventbot.service.PersonService;
+import io.quarkus.security.Authenticated;
+import io.quarkus.security.identity.SecurityIdentity;
 
+import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
@@ -15,22 +20,36 @@ public class PersonRest {
 
     PersonService pservice = new PersonService();
 
+    @Inject
+    SecurityIdentity identity;
+
+    @Authenticated
     @GET
     @Path("get/all")
     public Response getAllPersons() {
         return Response.ok().entity(Person.findAll().list()).build();
     }
 
+    @Authenticated
     @GET
     @Path("get/byName/{pname}")
     public Response getPersonByName(@PathParam("pname") String pname){
         return pservice.getPersonByName(pname);
     }
 
+    @Authenticated
     @GET
     @Path("get/byID/{id}")
     public Response getPersonByID(@PathParam("id") UUID id){
         return pservice.getPersonByID(id);
+    }
+
+    @Authenticated
+    @POST
+    @Transactional
+    @Path("create/{id}/{name}")
+    public Response createPerson(@PathParam("id") UUID id, @PathParam("name") String name) {
+        return pservice.createPerson(id, name);
     }
 
 }
